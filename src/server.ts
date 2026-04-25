f4b98fe97be0e840f46e341f457b2e7f6210de5d
Chloetrini <trinityegbukwu@yahoo.com>
2026-04-24 22:52:54 -0700

    fix cors for production

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       process.env.CLIENT_URL || "http://localhost:5173",
-      "https://nestfinderpro2-frontend.vercel.app",
+      "https://nestfinder-real-estate-ljlj.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
